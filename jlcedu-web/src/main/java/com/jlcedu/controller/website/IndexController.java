0ea4b75ca220ed4ea80d14b890a7ff190cafb6c1
Quincy.Ma <mq7253180@126.com>
2017-07-14 22:58:01 +0800

    xxx

--- jlcedu-web/src/main/java/com/jlcedu/controller/website/IndexController.java
+++ jlcedu-web/src/main/java/com/jlcedu/controller/website/IndexController.java
@@ -1,9 +1,13 @@
 package com.jlcedu.controller.website;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Enumeration;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.http.HttpResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,11 +31,22 @@ public class IndexController {
 
 	@RequestMapping("/test")
 	@ResponseBody
-	public String test(HttpServletRequest request) {
+	public String test(HttpServletRequest request, HttpResponse response) throws IOException {
 		Enumeration<String> names = request.getHeaderNames();
 		while(names.hasMoreElements()) {
 			String name = names.nextElement();
 			log.error(name+"-------"+request.getHeader(name));
+		}
+		InputStream in = null;
+		try {
+			in = request.getInputStream();
+			in = new BufferedInputStream(in);
+			byte[] buf = new byte[in.available()];
+			in.read(buf);
+			log.error("Content: "+new String(buf));
+		} finally {
+			if(in!=null)
+				in.close();
 		}
 		log.error("=============");
 		return "ssssssss";
