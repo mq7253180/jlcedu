0c57b8eab6d8549d551c2e45f46125b6b1d53d70
Quincy.Ma <mq7253180@126.com>
2017-07-17 22:42:39 +0800

    xxx

--- jlcedu-web/src/main/webapp/resource/quincy.js
+++ jlcedu-web/src/main/webapp/resource/quincy.js
@@ -6,47 +6,57 @@
 		}
 	}
 	$.fn.ajaxUploadFiles = function(s) {
+		var formData = new FormData();
+		var validationError = "";
+		var retVal = 1;
 		var path = $(this).val();
 		if(path==null||path.length==0) {
-			alert(s.errorMsg.notSelected);
-			return 0;
-		}
-		var formData = new FormData();
-		var files = $(this).prop("files");
-		for(var i=0;i<files.length;i++) {
-			var file = files[i];
-			var validFileType = false;
-			for(var j=0;j<s.acceptFileTypes.length;j++) {
-				var regExp = new RegExp(".+\."+s.acceptFileTypes[j]+"$", "i");
-				if(regExp.test(file.name)) {
-					validFileType = true;
+			validationError = s.errorMsg.notSelected;
+			retVal = 0;
+		} else {
+			var files = $(this).prop("files");
+			for(var i=0;i<files.length;i++) {
+				var file = files[i];
+				var validFileType = false;
+				for(var j=0;j<s.acceptFileTypes.length;j++) {
+					var regExp = new RegExp(".+\."+s.acceptFileTypes[j]+"$", "i");
+					if(regExp.test(file.name)) {
+						validFileType = true;
+						break;
+					}
+				}
+				if(!validFileType) {
+					validationError = s.errorMsg.acceptFileTypes;
+					retVal = -1;
 					break;
 				}
-			}
-			if(!validFileType) {
-				alert(s.errorMsg.acceptFileTypes);
-				return -1;
-			}
-			if(file.size>s.maxSize) {
-				alert(s.errorMsg.maxSize);
-				return -2;
-			}
-			formData.append("file"+i, file);
-		}
-		var req = window.XMLHttpRequest?new XMLHttpRequest():new ActiveXObject("Microsoft.XMLHTTP");
-		req.onreadystatechange = function() {
-			if(req.readyState==4) {
-				s.complete(req.status, req.responseText);
-				if(req.status==200) {
-					s.success(JSON.parse(req.responseText));
-				} else {
-					s.failure(req.status, req.responseText);
+				if(file.size>s.maxSize) {
+					validationError = s.errorMsg.maxSize;
+					retVal = -2;
+					break;
 				}
+				formData.append("file"+i, file);
 			}
-		};
-		req.open("POST", s.url, true);
-		req.setRequestHeader("x-requested-with", "XMLHttpRequest");
-		req.send(formData);
-		return 1;
+		}
+		if(retVal<1) {
+			s.validationFailed();
+			alert(validationError);
+		} else {
+			var req = window.XMLHttpRequest?new XMLHttpRequest():new ActiveXObject("Microsoft.XMLHTTP");
+			req.onreadystatechange = function() {
+				if(req.readyState==4) {
+					s.complete(req.status, req.responseText);
+					if(req.status==200) {
+						s.success(JSON.parse(req.responseText));
+					} else {
+						s.failure(req.status, req.responseText);
+					}
+				}
+			};
+			req.open("POST", s.url, true);
+			req.setRequestHeader("x-requested-with", "XMLHttpRequest");
+			req.send(formData);
+		}
+		return retVal;
 	};
 })(jQuery);
