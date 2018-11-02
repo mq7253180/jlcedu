5d99e5a82cd82945ea9b245776b78ac14419ec2e
Qiang Ma <mq7253180@126.com>
2018-11-02 22:09:37 +0800

    xxx

--- quincy-core/src/main/java/com/quincy/core/aop/CacheAop.java
+++ quincy-core/src/main/java/com/quincy/core/aop/CacheAop.java
@@ -21,6 +21,7 @@ import redis.clients.jedis.JedisPool;
 @Order(10)
 @Component
 public class CacheAop {
+	private final static String KEY_PREFIX = "CACHE_";
 	@Autowired
 	private JedisPool jedisPool;
 	
@@ -29,52 +30,53 @@ public class CacheAop {
 
     @Around("pointCut()")
     public Object doAround(ProceedingJoinPoint joinPoint) throws Throwable {
-    	MethodSignature methodSignature = (MethodSignature)joinPoint.getSignature();
+    		MethodSignature methodSignature = (MethodSignature)joinPoint.getSignature();
 		Class<?> clazz = joinPoint.getTarget().getClass();
 		Method method = clazz.getMethod(methodSignature.getName(), methodSignature.getParameterTypes());
 		Cache annotation = method.getAnnotation(Cache.class);
 		String keyStr = annotation.key().trim();
 		byte[] key = null;
 		if(keyStr.length()>0) {
-			key = keyStr.getBytes();
+			key = (KEY_PREFIX+keyStr).getBytes();
 		} else {
 			StringBuilder sb = new StringBuilder(100);
-    		sb.append(clazz.getName());
-    		sb.append(".");
-    		sb.append(methodSignature.getName());
-    		Class<?>[] clazzes = method.getParameterTypes();
-    		Object[] args = joinPoint.getArgs();
-    		if(args!=null&&args.length>0) {
-    			for(int i=0;i<args.length;i++) {
-    				Object arg = args[i];
-        			sb.append("_");
-        			sb.append(clazzes[i].getName());
-        			sb.append("#");
-        			sb.append(arg==null?"null":arg.toString().trim());
-        		}
-    		}
-    		key = sb.toString().getBytes();
+			sb.append(KEY_PREFIX);
+    			sb.append(clazz.getName());
+    			sb.append(".");
+    			sb.append(methodSignature.getName());
+    			Class<?>[] clazzes = method.getParameterTypes();
+    			Object[] args = joinPoint.getArgs();
+    			if(args!=null&&args.length>0) {
+    				for(int i=0;i<args.length;i++) {
+    					Object arg = args[i];
+    					sb.append("_");
+    					sb.append(clazzes[i].getName());
+    					sb.append("#");
+    					sb.append(arg==null?"null":arg.toString().trim());
+    				}
+    			}
+    			key = sb.toString().getBytes();
 		}
-    	Jedis jedis = null;
-    	try {
-    		jedis = jedisPool.getResource();
-    		byte[] cache = jedis.get(key);
-    		if(cache==null||cache.length==0) {
-        		Object retVal = joinPoint.proceed();
-        		if(retVal!=null) {
-        			jedis.set(key, CommonHelper.serialize(retVal));
-            		int expire = annotation.expire();
-            		if(expire>0) {
-                		jedis.expire(key, expire);
-            		}
-        		}
-        		return retVal;
-    		} else {
+		Jedis jedis = null;
+    		try {
+    			jedis = jedisPool.getResource();
+    			byte[] cache = jedis.get(key);
+    			if(cache==null||cache.length==0) {
+    				Object retVal = joinPoint.proceed();
+    				if(retVal!=null) {
+    					jedis.set(key, CommonHelper.serialize(retVal));
+    					int expire = annotation.expire();
+    					if(expire>0) {
+    						jedis.expire(key, expire);
+    					}
+    				}
+    				return retVal;
+    			} else {
                 return CommonHelper.unSerialize(cache);
+    			}
+    		} finally {
+    			if(jedis!=null)
+    				jedis.close();
     		}
-    	} finally {
-    		if(jedis!=null)
-    			jedis.close();
-    	}
     }
 }
