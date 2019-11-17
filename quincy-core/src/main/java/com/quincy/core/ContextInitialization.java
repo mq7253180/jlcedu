c9f98a76e20d2d46b78ae133f02de5c75c2d11bf
Qiang Ma <mq7253180@126.com>
2019-11-17 10:44:06 +0800

    xxx

---
+++ quincy-core/src/main/java/com/quincy/core/ContextInitialization.java
@@ -0,0 +1,24 @@
+package com.quincy.core;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.PropertySource;
+
+import lombok.extern.slf4j.Slf4j;
+
+//@PropertySource("classpath:core.properties")
+@Configuration
+@Slf4j
+public class ContextInitialization {
+	@Value("#{'${locales}'.split(',')}")
+	private String[] supportedLocales;
+
+	@PostConstruct
+	public void init() {
+		for(String l:supportedLocales) {
+			log.warn("SUPPORTED_LOCALE--------------{}", l);
+		}
+	}
+}
